081f769d854bfcd952d0a575216f19e09489db0f
Rashadul Haque <rashadulhaque@Rashaduls-MacBook-Air.local>
2023-06-14 17:49:02 +0200

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import myImg from "../assets/images/rashadul.png";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <header className="app-header border shadow-md text-white p-8 flex sm:justify-center items-center sm:items-start relative sm:h-[300px]">
+      <img
+        className="profile-img w-[240px] h-[240px] sm:w-40 sm:h-40 md:w-64 md:h-64 p-0 rounded-tl-lg rounded-tr-lg"
+        src={myImg}
+        alt="Rashadul Haque"
+      />
+      <div className="flex flex-col justify-center sm:justify-start ml-4 sm:ml-0">
+        <h1 className="text-2xl sm:text-xl text-center sm:text-start sm:ps-0">
+          RASHADUL HAQUE
+        </h1>
+        <p className="text-5xl sm:text-4xl font-bold sm:w-64 md:w-64 mt-4 text-center sm:text-start">
+          Full Stack Developer
+        </p>
+      </div>
+      <div className="flex justify-center items-center gap-6 absolute top-4 right-48 md:right-40 sm:static">
+        <a href="https://github.com/your-github-url">
+          <FaGithub size={24} />
+        </a>
+        <a href="https://linkedin.com/your-linkedin-url">
+          <FaLinkedin size={24} className="rounded-full" />
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
