5c5a745e6ab55055f7ca9ade188130e3c80430cd
Rashadul Haque <rashadulhaque@Rashaduls-MacBook-Air.local>
2023-06-19 00:06:15 +0200

    added projects data and links

--- src/components/card/ProjectCard.tsx
+++ src/components/card/ProjectCard.tsx
@@ -1,28 +1,53 @@
-import React, { useState } from "react";
+
+import React, { useEffect, useState } from "react";
 import { ProjectCardProps } from "../../types";
+
 const ProjectCard = ({ item }: ProjectCardProps) => {
   const [hover, setHover] = useState(false);
   const imagePath = require(`../../assets/images/${item.image}`);
+
+  useEffect(() => {
+    const handleTouchMove = () => {
+      setHover(false);
+    };
+
+    window.addEventListener("touchmove", handleTouchMove);
+
+    return () => {
+      window.removeEventListener("touchmove", handleTouchMove);
+    };
+  }, []);
 
   const cardStyle: React.CSSProperties = {
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
-    padding: "2rem",
+    padding: "0rem 2rem",
     borderRadius: "0.5rem",
     boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
     transition: "transform 0.3s ease",
     transform: hover ? "scale(0.95)" : "scale(1)",
-    cursor: "pointer",
     height: "620px",
     background: "#547D8E",
   };
 
   const overlayStyle: React.CSSProperties = {
-    backgroundColor: hover ? "#003333" : "transparent",
-    color: hover ? "white" : "#FEF17F",
-    padding: "0.5rem",
+    backgroundColor: "#000",
+    color: "#FFF",
+    padding: "0.2rem 1.5rem ",
+    borderRadius: "0.25rem",
+    marginBottom: "1.5rem",
+    textAlign: "center",
+    fontSize: "1.5rem",
+    fontWeight: "bold",
+    transition: "transform 0.3s ease",
+    transform: hover ? "scale(0.95)" : "scale(1)",
+  };
+  const linksStyle: React.CSSProperties = {
+   backgroundColor: hover? "#FEF17F": "transparent",
+    color: hover? "#000000" : "#FEF17F",
+    padding: "0.5rem 1.5rem ",
     borderRadius: "0.25rem",
     marginBottom: "1rem",
     textAlign: "center",
@@ -39,35 +64,72 @@ const ProjectCard = ({ item }: ProjectCardProps) => {
     textAlign: "center",
   };
 
+  const imageContainerStyle: React.CSSProperties = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: "240px", // Adjust the desired height here
+    marginBottom: "4px", // Adjust the desired margin here
+  };
+
   return (
-    <a href={item.url} target="_blank" rel="noreferrer">
-      <div
-        style={cardStyle}
-        className={`hover:border-gray-800 ${hover ? "border-gray-800" : ""}`}
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-        onTouchStart={() => setHover(true)}
-        onTouchEnd={() => setHover(false)}
-      >
-        <div className="project-overlay" style={overlayStyle}>
+    <div
+      style={cardStyle}
+      className={`hover:border-gray-800 ${hover ? "border-gray-800" : ""}`}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      onTouchStart={() => setHover(!hover)}
+    >
+      {!hover && (
+        <div className="project-overlay" style={linksStyle}>
           {item.overlay}
         </div>
-        <img
-          className="w-3/4 h-auto mb-4"
-          src={imagePath}
-          alt={item.overlay}
-        />
-        <h2 className="text-white">{item.title}</h2>
-        <p className="py-4 text-[#FEF17F]">{item.description}</p>
-        <div className="flex justify-start items-center flex-wrap w-full">
-          {item.tags.map((tag: any) => (
-            <span key={tag} style={tagStyle}>
-              {tag}
-            </span>
-          ))}
+      )}
+      {hover && (
+        <a
+          className="text-xs"
+          href={item.github_url}
+          target="_blank"
+          rel="noreferrer"
+          style={linksStyle}
+          onTouchStart={(e) => {
+            e.stopPropagation();
+            setHover(true);
+          }}
+        >
+          <span className="z-30">🔗</span> <span className="z-30">CODE</span>
+        </a>
+      )}
+        <div className="flex justify-center items-center relative" style={imageContainerStyle}>
+        {hover && (
+          <a
+            href={item.url}
+            target="_blank"
+            rel="noreferrer"
+            style={overlayStyle}
+            className="absolute top-0 right-0 w-full h-full flex justify-center items-center gap-1 opacity-70 "
+            onTouchStart={(e) => {
+              e.stopPropagation();
+              setHover(true);
+            }}
+          >
+            <span className="z-30">🔗</span> <span className="z-30">Link</span>
+          </a>
+        )}
+        <img className="w-3/4 h-auto mb-4 rounded-md" src={imagePath} alt={item.overlay} />
         </div>
+      
+      <h2 className="text-white">{item.title}</h2>
+      <p className="py-4 text-[#FEF17F]">{item.description}</p>
+      <div className="flex justify-start items-center flex-wrap w-full">
+        {item.tags.map((tag: any) => (
+          <span key={tag} style={tagStyle}>
+            {tag}
+          </span>
+        ))}
       </div>
-    </a>
+    </div>
   );
 };
 
